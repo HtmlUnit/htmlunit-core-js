ee2d850659e9271568c426e2234d6cd5250c46fe
Ahmed Ashour <asashour@yahoo.com>
2012-11-11 10:11:41 +0300

    MemberBox: add detailed description in the exception thrown if parameters are different than declared arguments.

---
+++ src/net/sourceforge/htmlunit/MemberBoxTest.java
@@ -0,0 +1,55 @@
+package net.sourceforge.htmlunit;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextAction;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+/**
+ * Tests for {@link org.mozilla.javascript.MemberBox}
+ *
+ * @author Ahmed Ashour
+ */
+public class MemberBoxTest extends TestCase {
+
+    @Test
+	public void testOnlyGetterError() {
+		final String script = "var o = new MyHostObject(); o.someFunction(new Object())";
+		
+		final ContextAction action = new ContextAction() {
+			public Object run(final Context cx) {
+				try {
+					Scriptable scope = cx.initStandardObjects();
+                    ScriptableObject.defineClass(scope, MyHostObject.class);
+
+					cx.evaluateString(scope, script, "test_script", 1, null);
+					fail("An exception should be thrown");
+				}
+				catch (final Exception e) {
+				    Assert.assertEquals("Exception invoking MyHostObject.jsFunction_someFunction() "
+				            + "with arguments [NativeObject]",
+				            e.getMessage());
+				}
+                return null;
+			}
+		};
+
+		Utils.runWithAllOptimizationLevels(action);
+	}
+
+	public static class MyHostObject extends ScriptableObject {
+		@Override
+		public String getClassName() {
+			return getClass().getSimpleName();
+		}
+
+		public void jsFunction_someFunction(Function a) {
+		}
+
+	}
+}
