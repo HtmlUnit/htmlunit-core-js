43c7b8ccd25a8ae7de8680e43c7d2bae2d26a7d0
ahmed.ashour <ahmed.ashour@ITD-DLLP-0051.CORP.SA.ZAIN.COM>
2013-12-18 12:17:44 +0300

    Test case for parseInt(), bug #1563

---
+++ src/net/sourceforge/htmlunit/NativeGlobalTest.java
@@ -0,0 +1,56 @@
+package net.sourceforge.htmlunit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextAction;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
+
+/**
+ * Tests for NativeGlobal object.
+ *
+ * @author Ahmed Ashour
+ */
+public class NativeGlobalTest {
+
+	/**
+	 * Test for parseInt.
+	 */
+	@Test
+	public void parseInt() throws Exception {
+	    doTest(0, "parseInt('08')", false);
+        doTest(8, "parseInt('08')", true);
+
+        doTest(0, "parseInt('08', 8)", false);
+        doTest(0, "parseInt('08', 8)", true);
+
+        doTest(Double.NaN, "parseInt('8', 8)", false);
+        doTest(Double.NaN, "parseInt('8', 8)", true);
+
+        doTest(8, "parseInt('8')", false);
+        doTest(8, "parseInt('8')", true);
+	}
+
+    private void doTest(final double expected, final String src, final boolean contextFeature) throws Exception {
+		final ContextFactory myContextFactory = new ContextFactory() {
+			protected boolean hasFeature(final Context cx, final int featureIndex) {
+				if (Context.FEATURE_HTMLUNIT_PARSE_INT_RADIX_10 == featureIndex) {
+					return contextFeature;
+				}
+				return super.hasFeature(cx, featureIndex);
+			};
+		};
+		
+		final ContextAction action = new ContextAction() {
+			public Object run(final Context cx) {
+				final Scriptable scope = cx.initStandardObjects();
+				Object result = cx.evaluateString(scope, src, "test.js", 0, null);
+                Assert.assertEquals(expected, result);
+				return null;
+			}
+		};
+		Utils.runWithAllOptimizationLevels(myContextFactory, action);
+	}
+
+}
