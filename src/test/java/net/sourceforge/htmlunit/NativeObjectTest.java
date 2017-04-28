1318dcc1d42e1e8c8690ce6bf37f76bf25ee3679
Ahmed Ashour <asashour@yahoo.com>
2017-04-28 20:41:29 +0200

    Add test case

--- src/test/java/net/sourceforge/htmlunit/NativeObjectTest.java
+++ src/test/java/net/sourceforge/htmlunit/NativeObjectTest.java
@@ -47,6 +47,32 @@ public class NativeObjectTest {
      * @throws Exception if the test fails
      */
     @Test
+    public void assignNull() throws Exception {
+        final String script = "function test() {\n"
+                + "  var copy = Object.assign({}, null);\n"
+                + "  output += copy.a;\n"
+                + "}\n"
+                + "var output = '';\n"
+                + "test();\n"
+                + "output";
+        
+        final ContextAction action = new ContextAction() {
+            @Override
+            public Object run(final Context cx) {
+                final Scriptable scope = cx.initStandardObjects();
+                final Object result = cx.evaluateString(scope, script, "test.js", 1, null);
+                assertEquals("undefined", result);
+                return null;
+            }
+        };
+
+       Utils.runWithAllOptimizationLevels(action);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
     public void getPrototypeOfString() throws Exception {
         getPrototypeOfString("", "''", true);
     }
