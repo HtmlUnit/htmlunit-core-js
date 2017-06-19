67a661e4d26d3ce1335af4b559d0333f05fa4a8b
Ahmed Ashour <asashour@yahoo.com>
2017-06-19 13:17:45 +0200

    Add test case for delcaration as Function Expression

--- src/test/java/net/sourceforge/htmlunit/FunctionTest.java
+++ src/test/java/net/sourceforge/htmlunit/FunctionTest.java
@@ -5,11 +5,14 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextAction;
+import org.mozilla.javascript.EcmaError;
 import org.mozilla.javascript.Scriptable;
 
 /**
  * Unit tests for Function.
+ *
  * @author Marc Guillemot
+ * @author Ahmed Ashour
  */
 public class FunctionTest {
     
@@ -36,4 +39,33 @@ public class FunctionTest {
         };
         Utils.runWithAllOptimizationLevels(action);
     }
- }
+
+    @Test
+    public void varDefinedLater() {
+        String script = "var output = '';\n"
+                + "output += t;\n"
+                + "var t = 1;\n"
+                + "output";
+        assertEvaluates("undefined", script);
+    }
+
+    @Test(expected = EcmaError.class)
+    public void varUsedLater() {
+        String script = "var output = '';\n"
+                + "output += t;\n"
+                + "!function t() {output += 'hi';}();\n"
+                + "output";
+        assertEvaluates("undefined", script);
+    }
+
+    @Test
+    public void functionHasNameOfVar() {
+        String script = "var output = '';\n"
+                + "output += t;\n"
+                + "var t = 1;\n"
+                + "!function t() {output += 'hi';}();\n"
+                + "output";
+        assertEvaluates("undefinedhi", script);
+    }
+
+}
