c6474070a170fab9fee6e8669531907b3935e0bc
Ronald Brill <rbri@rbri.de>
2023-12-30 12:24:20 +0100

    add tests for Reflect ctor args

---
+++ src/test/java/org/htmlunit/corejs/ReflectTest.java
@@ -0,0 +1,75 @@
+package org.htmlunit.corejs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.htmlunit.corejs.javascript.Context;
+import org.htmlunit.corejs.javascript.ContextAction;
+import org.htmlunit.corejs.javascript.Scriptable;
+import org.htmlunit.corejs.javascript.ScriptableObject;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for NativeReflect
+ *
+ * @author Ronald Brill
+ */
+public class ReflectTest {
+
+    @Test
+    public void constructor() {
+        final String script =
+                "  var res = '';\n"
+                + "function foo(a, b) {\n"
+                + "  res += 'foo - ';\n"
+                + "  for (let i = 0; i < arguments.length; i++) {\n"
+                + "    res += arguments[i] + ' ';\n"
+                + "  }\n"
+                + "}\n"
+
+                + "Reflect.construct(foo, [1, 2]);\n"
+                + "res;";
+
+        test(script, "foo - 1 2 ");
+    }
+
+    @Test
+    public void constructorWithTarger() {
+        final String script =
+                "  var res = '';\n"
+                + "function foo(a, b) {\n"
+                + "  res += 'foo - ';\n"
+                + "  for (let i = 0; i < arguments.length; i++) {\n"
+                + "    res += arguments[i] + ' ';\n"
+                + "  }\n"
+                + "}\n"
+                + "function bar(a, b) {\n"
+                + "  res += 'bar - ';\n"
+                + "  for (let i = 0; i < arguments.length; i++) {\n"
+                + "    res += arguments[i] + ' ';\n"
+                + "  }\n"
+                + "}\n"
+
+                + "Reflect.construct(foo, [6, 7, 8], bar);\n"
+                + "res;";
+
+        test(script, "foo - 6 7 8 ");
+    }
+
+    private static void test(final String script, final Object expected) {
+        final ContextAction<Object> action = new ContextAction<Object>() {
+            @Override
+            public Object run(final Context cx) {
+                try {
+                    Scriptable scope = cx.initSafeStandardObjects();
+                    final Object o = cx.evaluateString(scope, script, "test_script", 1, null);
+                    assertEquals(expected, o);
+                    return o;
+                } catch (final Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Utils.runWithAllOptimizationLevels(action);
+    }
+}
