aa6f1d06f9718a73defe61348ffc6f75d8a31ba4
Ronald Brill <rbri@rbri.de>
2024-06-16 14:57:08 +0200

    latest rhino

--- src/test/java/org/htmlunit/corejs/NativeObjectTest.java
+++ src/test/java/org/htmlunit/corejs/NativeObjectTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
  * Tests for NativeObject.
  *
  * @author Ahmed Ashour
+ * @author Ronald Brill
  */
 public class NativeObjectTest {
 
@@ -65,5 +66,39 @@ public class NativeObjectTest {
         };
 
        Utils.runWithAllOptimizationLevels(action);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void definePropertyUsingConsString() throws Exception {
+        final String script =
+            "  'use strict';\n"
+            + "  var log = '';\n"
+            + "  var f = function () {};\n"
+            + "  var a1='proto';\n"
+            + "  var p = a1 + 'type';\n"
+            + "  log = log + 'before: ' + f.prototype;\n"
+            + "  Object.defineProperty(f, p, {});\n"
+            + "  log = log + ' / after: ' + f.prototype;\n"
+            + "  var p = new f();\n"
+            + "  log = log + ' / ' + (p instanceof f);";
+
+        final ContextAction<Object> action = new ContextAction<Object>() {
+            @Override
+            public Object run(final Context cx) {
+                try {
+                    Scriptable scope = cx.initSafeStandardObjects();
+                    final Object o = cx.evaluateString(scope, script, "test_script", 1, null);
+                    assertEquals("before: [object Object] / after: [object Object] / true", o);
+                    return o;
+                } catch (final Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Utils.runWithAllOptimizationLevels(action);
     }
 }
