2e7f83c7528a66058aa479d3cd965aa117d475fa
Ronald Brill <rbri@rbri.de>
2025-01-03 19:31:26 +0100

    test moved to rhino

--- src/test/java/org/htmlunit/corejs/ExceptionMessageTest.java
+++ src/test/java/org/htmlunit/corejs/ExceptionMessageTest.java
@@ -17,8 +17,9 @@ import org.htmlunit.corejs.javascript.ScriptableObject;
 
 /**
  * Test that exception messages contain rich information.
- * Unit tests for <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=604674">bug 604674</a>.
+ *
  * @author Marc Guillemot
+ * @author Ronald Brill
  */
 public class ExceptionMessageTest {
 
@@ -59,68 +60,5 @@ public class ExceptionMessageTest {
         };
 
         Utils.runWithAllOptimizationLevels(action);
-    }
-
-    /**
-     * Unit test for bug 604674
-     * https://bugzilla.mozilla.org/show_bug.cgi?id=604674
-     */
-    @Test
-    public void onlyGetterError() {
-        onlyGetterError(Context.FEATURE_STRICT_MODE);
-    }
-
-    private static void onlyGetterError(final int feature) {
-        final ContextFactory cf = new ContextFactory() {
-            @Override
-            protected boolean hasFeature(Context cx, int featureIndex) {
-                if (Context.FEATURE_STRICT_MODE == featureIndex) {
-                    return true;
-                }
-                return super.hasFeature(cx, featureIndex);
-            }
-        };
-
-        final String script = "o.readonlyProp = 123";
-
-        final ContextAction<Object> action = new ContextAction<Object>() {
-            @Override
-            public Object run(final Context cx) {
-                try {
-                    Scriptable scope = cx.initSafeStandardObjects();
-                    final MyHostObject prototype = new MyHostObject();
-                    ScriptableObject.defineClass(scope, MyHostObject.class);
-                    final Method readMethod = MyHostObject.class.getMethod("jsxGet_x");
-                    prototype.defineProperty("readonlyProp", null, readMethod , null, ScriptableObject.EMPTY);
-
-                    ScriptableObject.defineProperty(scope, "o", prototype, ScriptableObject.DONTENUM);
-
-                    cx.evaluateString(scope, script, "test_script", 1, null);
-                    throw new RuntimeException("Should have failed!");
-                }
-                catch (final EcmaError e) {
-                    assertEquals("TypeError: Cannot set property [MyHostObject].readonlyProp that has only a getter to value '123'. (test_script#1)", e.getMessage());
-                    return null;
-                }
-                catch (final Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        };
-
-        Utils.runWithAllOptimizationLevels(cf, action);
-    }
-
-    public static class MyHostObject extends ScriptableObject {
-        private int x;
-
-        @Override
-        public String getClassName() {
-            return getClass().getSimpleName();
-        }
-
-        public int jsxGet_x() {
-            return x;
-        }
     }
 }
