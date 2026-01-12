9faea341c6d657a2a08a708b87512c25a242f857
Ronald Brill <rbri@rbri.de>
2026-01-12 16:47:54 +0100

    more export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,11 @@ module org.htmlunit.corejs {
     requires java.desktop;
 
     exports org.htmlunit.corejs.javascript;
+    exports org.htmlunit.corejs.javascript.debug;
+    exports org.htmlunit.corejs.javascript.json;
+    exports org.htmlunit.corejs.javascript.regexp;
+    exports org.htmlunit.corejs.javascript.tools.debugger;
+    exports org.htmlunit.corejs.javascript.typedarrays;
 
     // Service Loader Support
     uses org.htmlunit.corejs.javascript.NullabilityDetector;
